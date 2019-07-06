b7d8d74facd8e549f163bc5088102c27e82b3df6
Nhat Linh <o0onhatlinho0o@gmail.com>
2019-07-06 16:37:19 +0700

    No 2 Change Name

--- GameCaro/MainGame.Designer.cs
+++ GameCaro/MainGame.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GameCaro
 {
-    partial class Form1
+    partial class MainGame
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "MainGame";
+            this.Text = "GameCaro";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- GameCaro/MainGame.cs
+++ GameCaro/MainGame.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GameCaro
 {
-    public partial class Form1 : Form
+    public partial class MainGame : Form
     {
-        public Form1()
+        public MainGame()
         {
             InitializeComponent();
         }
